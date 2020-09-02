3c54f45ebd52ce3c912833edbe1d2845f4105f13
Microsft-Wrd <60941772+MicrosoftWord12@users.noreply.github.com>
2020-09-02 22:06:29 +0100

    Accounts
    - we have some functionality
    - require .txt files for banking
    - created CreateAccount class

--- src/Application.java
+++ src/Application.java
@@ -3,28 +3,51 @@ import java.util.Scanner;
 public class Application {
     Scanner input = new Scanner(System.in);
 
+//    Custom Classes
+    CreateAccount createAccount = new CreateAccount();
+
     public static void main(String[] args) {
         Application app = new Application();
-        CurrentAccount tomMumford = new CurrentAccount("Thomas", "Mumford", "thm37@aber.ac.uk", 123456789);
+//        CurrentAccount tomMumford = new CurrentAccount("Thomas", "Mumford", "thm37@aber.ac.uk", 123456789);
         app.run();
 
         //add code to run
-        System.out.println(tomMumford.toString());
+//        System.out.println(tomMumford.toString());
     }
 
     public void run(){
-        System.out.println("Welcome to your bank");
-        System.out.println("Do you need to setup a current account/Savings account?");
+        System.out.println("Welcome to your Online bank");
+        System.out.println("Create Current Account: Setup Current Account");
+        System.out.println("Create Savings Account: Setup Savings Account");
+        System.out.println("View Balance: view balance");
+        System.out.println("Add Funds: add funds");
+
         String account = input.nextLine();
         switch (account){
-            case "setup":
+            case "setup current account":
+
+            case "setup savings account":
+                System.out.println("What is your email Address?");
+                createAccount.setEmailAddress(input.nextLine());
+
+                System.out.println("What is your first name");
+                createAccount.setFirstName(input.nextLine());
+
+                System.out.println("What is your last name");
+                createAccount.setLastName(input.nextLine());
+
                 System.out.println("Account Created");
                 break;
 
-            case "no":
-                System.out.println("No");
-            }
+            case "view balance":
+                createAccount.getBalance();
 
+
+            case "add funds":
+                System.out.println("How much would you like to add?");
+                createAccount.setBalance(input.nextInt());
+                System.out.println(createAccount.getBalance());
+        }
         }
     }
 
---
+++ src/CreateAccount.java
@@ -0,0 +1,46 @@
+public class CreateAccount{
+    private String firstName;
+    private String lastName;
+    private String emailAddress;
+    private int balance;
+
+//    public void makeAccount(String firstName, String lastName, String emailAddress){
+//        this.firstName = firstName;
+//        this.lastName = lastName;
+//        this.emailAddress = emailAddress;
+//    }
+
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public long getBalance() {
+        return balance;
+    }
+
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
+
+}
