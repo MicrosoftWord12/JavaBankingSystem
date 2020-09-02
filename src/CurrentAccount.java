e69abac0a29f4c74c506c3aa8cc5a9f3b87d21ce
iappletom <thomas12376@hotmail.com>
2020-09-02 14:43:52 +0100

    Added Balance to CurrentAccount.java
    Signed-off-by: iappletom <thomas12376@hotmail.com>

--- src/CurrentAccount.java
+++ src/CurrentAccount.java
@@ -1,6 +1,15 @@
 public class CurrentAccount extends AccountInfo {
+    private float balance;
 
     public CurrentAccount(String firstName, String lastName, String emailAddress, long accountNo) {
         super(firstName, lastName, emailAddress, accountNo);
+    }
+
+    public float getBalance() {
+        return balance;
+    }
+
+    public void setBalance(float balance) {
+        this.balance = balance;
     }
 }
